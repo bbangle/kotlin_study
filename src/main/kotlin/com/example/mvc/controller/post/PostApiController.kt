d7f1ddbf1cc3e5d1f2be52b554e69b559b0ea716
bbangle <gr8hangul@gmail.com>
2026-01-12 23:43:31 +0900

    코틀린 POST방식

---
+++ src/main/kotlin/com/example/mvc/controller/post/PostApiController.kt
@@ -0,0 +1,33 @@
+package com.example.mvc.controller.post
+
+import com.example.mvc.model.http.UserRequest
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api")
+class PostApiController {
+
+    @PostMapping("/post-mapping")
+    fun postMapping():String {
+        return "post-mapping"
+    }
+
+    @RequestMapping("/post-request-mapping", method = [RequestMethod.POST])
+    fun requestMapping():String {
+        return "post-request-mapping"
+    }
+
+    // Object mapper
+    // json -> object
+    // object -> json
+    @PostMapping("/post-mapping/object")
+    fun postMappingObject(@RequestBody userRequest: UserRequest): UserRequest {
+        println(userRequest)
+        userRequest.name += ": Post"
+        return userRequest
+    }
+}
